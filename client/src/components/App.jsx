cbcc4c1a48a07e4a63ad7ef44016ea7654b7030d
joshxcho <joshxcho123@gmail.com>
2019-09-22 19:00:06 -0700

    Implement Content component to render logo and date

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Headers from './Headers';
+import Content from './Content';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Headers />
+        <Content />
       </div>
     );
   }
---
+++ client/src/components/Content.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Logo from '../assets/logo/logo.png';
+
+const Content = () => (
+  <div className="main-container">
+    <div className="content-wrapper">
+      <div className="logo">
+        <img src={Logo} alt="logo" />
+      </div>
+      <div className="content-date">Coming 06.26.19</div>
+    </div>
+  </div>
+);
+export default Content;
