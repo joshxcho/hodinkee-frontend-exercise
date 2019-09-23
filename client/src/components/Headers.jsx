cb1d36aadfed9435f6c229643c8b4df83a99f350
joshxcho <joshxcho123@gmail.com>
2019-09-22 19:07:37 -0700

    Add a feature to enable link to Hodinkee shop

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 
 export default class Headers extends Component {
+  handleClickShop = () => {
+    window.location.assign('http://shop.hodinkee.com');
+  };
   render() {
     return (
       <div className="headers-container">
-        <button className="headers-icon" type="button">
-          SHARE
+        <button className="headers-icon" onClick={this.handleClickShop} type="button">
+          H SHOP
         </button>
         <button className="headers-icon" type="button">
-          H SHOP
+          SHARE
         </button>
       </div>
     );
