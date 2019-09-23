ea73641fcd514a84ed679a791e384b0b528da6ce
joshxcho <joshxcho123@gmail.com>
2019-09-22 19:19:29 -0700

    Add icons to Headers buttons

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 
+import { faShare } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 export default class Headers extends Component {
   handleClickShop = () => {
     window.location.assign('http://shop.hodinkee.com');
   };
+
   render() {
     return (
       <div className="headers-container">
         <button className="headers-icon" onClick={this.handleClickShop} type="button">
-          H SHOP
+          &#60; H SHOP
         </button>
         <button className="headers-icon" type="button">
-          SHARE
+          SHARE <FontAwesomeIcon icon={faShare} />
         </button>
       </div>
     );
